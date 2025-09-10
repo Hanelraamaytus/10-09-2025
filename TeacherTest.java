40f194c53e436d028f87f9bd637acc4a3427b051
Hanelraamaytus <hanelra_tian_34rpl@student.smktelkom-mlg.sch.id>
2025-09-10 19:43:15 +0700

    Add files via upload

---
+++ TeacherTest.java
@@ -0,0 +1,16 @@
+public class TeacherTest {
+    public static void main(String[] args) {
+        //constructor
+        Teachet Teacher1 = new Teachet();   
+        Teacher1.print();
+
+        Teachet Teacher2 = new Teachet();
+        Teacher2.setsubject("Science");
+        Teacher2.setname("alice");
+        Teacher2.setage(28);
+        Teacher2.setsalary(5500.0);
+
+        
+         } 
+    }
+
